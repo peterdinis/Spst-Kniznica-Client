aba8a6d527c4a97e484617d2f1da9ad8b66daa06
Peterdinis <peterdinis0@pobox.sk>
2022-07-05 14:29:11 +0200

    FOO

--- src/api/mutations/authMutations.ts
+++ src/api/mutations/authMutations.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LoginUserI, RegisterUserI } from "../interfaces/IAuth";
 
 const api = axios.create({
-    baseURL: "http://localhost:3333/api"
+    baseURL: "http://localhost:3001"
   });
 
 export const registerUser = async(dataI: RegisterUserI) => {
