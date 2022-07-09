eb41d759cd8ba735dcb95888bdae82fbcdabab11
Peterdinis <peterdinis0@pobox.sk>
2022-07-09 18:00:31 +0200

    New helper component added

---
+++ src/components/borrowing/styles.ts
@@ -0,0 +1,15 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+  }),
+);
