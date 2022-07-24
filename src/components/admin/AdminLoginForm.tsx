538f4eb1ea16b27369e6c683d3a81e8520979853
Peterdinis <peterdinis0@pobox.sk>
2022-07-24 11:54:29 +0200

    Updating forms teacher and admin

--- src/components/admin/AdminLoginForm.tsx
+++ src/components/admin/AdminLoginForm.tsx
@@ -5,6 +5,80 @@ function AdminLoginForm() {
   return (
     <>
       <Header name="Admin prihlásenie" />
+      <form>
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <div className="mb-4">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Email
+            </label>
+            <input
+              className="emailInput shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Password
+            </label>
+            <input
+              className="emailInput shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Meno
+            </label>
+            <input
+              className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="Meno"
+              type="text"
+              placeholder="Meno"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Priezvisko
+            </label>
+            <input
+              className="passwordInput shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="Priezvisko"
+              type="text"
+              placeholder="Priezvisko"
+            />
+          </div>
+          <div>
+            <button className="reg registerButton" type="submit">
+              Registrácia
+            </button>
+            <div>
+              <a
+                className="mt-4 inline-block align-baseline font-bold text-2xl text-blue hover:text-blue-darker"
+                href="/student/login"
+              >
+                Prihlásenie tu
+              </a>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   )
 }
