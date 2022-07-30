309c57c2f09bc3588ce4edabe4808db75797af22
Peterdinis <peterdinis0@pobox.sk>
2022-07-30 11:14:25 +0200

    Updating components

---
+++ src/components/admin/ChatingRoom.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChatingRoom() {
+  return (
+    <div>ChatingRoom</div>
+  )
+}
+
+export default ChatingRoom
--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -4,3 +4,5 @@ export const AdminLoginForm = React.lazy(() =>import("./AdminLoginForm"));
 export const AdminProfile = React.lazy(() =>import("./AdminProfile"));
 export const AdminRegisterForm = React.lazy(() =>import("./AdminRegisterForm"));
 export const ApplicationMonitoring = React.lazy(() =>import("./ApplicationMonitoring"));
+export const JoinRoom = React.lazy(() =>import("./JoinRoom"));
+export const ChatingRoom = React.lazy(() =>import("./ChatingRoom"));
